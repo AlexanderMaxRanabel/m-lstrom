d72e2cca4941b9886ec6f9a73ecd17638a29495a
AlexanderMaxRanabel <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-03-28 21:33:17 +0300

    Updated Devices.rs

--- src/server/handlers/devices.rs
+++ src/server/handlers/devices.rs
@@ -1,1 +1,90 @@
-// I have no idea what to write here PLEASE HELP ME
+use std::net::SocketAddr;
+
+pub struct Device {
+    pub id: String,
+    pub address: SocketAddr,
+    pub settings: DeviceSettings,
+}
+
+pub struct DeviceSettings {
+    // Define device settings here
+}
+
+impl Device {
+    pub fn new(id: String, address: SocketAddr) -> Self {
+        Self {
+            id,
+            address,
+            settings: DeviceSettings::default(),
+        }
+    }
+
+    pub fn connect(&self) -> Result<(), String> {
+        // Connect to device
+        Ok(())
+    }
+
+    pub fn disconnect(&self) -> Result<(), String> {
+        // Disconnect from device
+        Ok(())
+    }
+
+    pub fn update_settings(&mut self, settings: DeviceSettings) -> Result<(), String> {
+        // Update device settings
+        self.settings = settings;
+        Ok(())
+    }
+
+    pub fn get_data(&self) -> Result<Vec<u8>, String> {
+        // Get data from device
+        Ok(vec![])
+    }
+
+    pub fn send_data(&self, data: Vec<u8>) -> Result<(), String> {
+        // Send data to device
+        Ok(())
+    }
+}
+
+pub struct DeviceManager {
+    devices: Vec<Device>,
+}
+
+impl DeviceManager {
+    pub fn new() -> Self {
+        Self { devices: vec![] }
+    }
+
+    pub fn add_device(&mut self, device: Device) {
+        self.devices.push(device);
+    }
+
+    pub fn remove_device(&mut self, id: &str) -> Result<(), String> {
+        if let Some(index) = self.devices.iter().position(|d| d.id == id) {
+            self.devices.remove(index);
+            Ok(())
+        } else {
+            Err(format!("Device with id {} not found", id))
+        }
+    }
+
+    pub fn get_device(&self, id: &str) -> Result<&Device, String> {
+        if let Some(device) = self.devices.iter().find(|d| d.id == id) {
+            Ok(device)
+        } else {
+            Err(format!("Device with id {} not found", id))
+        }
+    }
+
+    pub fn get_devices(&self) -> &Vec<Device> {
+        &self.devices
+    }
+}
+
+impl Default for DeviceSettings {
+    fn default() -> Self {
+        Self {
+            // Set default device settings here
+        }
+    }
+}
